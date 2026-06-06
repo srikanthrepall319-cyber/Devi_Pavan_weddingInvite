181651d1465b906df7b7a94c99368618f54a14ff
srikanth3197 <srikanthrepall319@gmail.com>
2026-06-06 15:40:54 +0100

    error fix

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,5 +1,7 @@
-import { LoadingScreen } from "@/components/loading-screen";
-
 export default function Loading() {
-  return <LoadingScreen />;
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-[#2b0d16]">
+      <div className="text-gold">Loading...</div>
+    </div>
+  );
 }
